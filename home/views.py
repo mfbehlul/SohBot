6f7a470ca91c55a0725db6a9f93d67b56f99d332
Fatih Behlül <40476498+mfbehlul@users.noreply.github.com>
2021-05-15 21:05:32 +0300

    home view second edit.

--- home/views.py
+++ home/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 
 def home_view(request):
-    return HttpResponse("<b>SohBot.</b>")
+    return HttpResponse("<b>SohBot</b>")
